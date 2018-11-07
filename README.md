da37e5440b60ab4d55da556eaa8e40da979c3345
Alexander Burakovskiy <alexander.burakovskiy@gmail.com>
2018-11-07 09:03:03 +0200

    search movies

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# api-themoviedb
+# The Movie Database GraphQL wrapper
+
 A simple GraphQL server in Node.js that returns movie results based on search string
+
+## Running the app
+
+You'll need [themoviedb.org API key](https://www.themoviedb.org/documentation/api)
+
+Run the app with:
+
+```sh
+npm install
+THEMOVIEDB_API_KEY=<api key> npm start
+```
---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const express_graphql = require("express-graphql");
+const { makeExecutableSchema } = require("graphql-tools");
+const fetch = require("node-fetch");
+const cors = require("cors");
+
+const THEMOVIEDB_API_PATH = "https://api.themoviedb.org/3";
+
+const typeDefs = String.raw`
+  type Query {
+    movies(query: String!): [Movie]
+    movie(id: Int!): Movie
+  }
+  type Movie {
+    id: Int
+    title: String
+    release_date: String
+    popularity: Float
+    overview: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    movies: (root, args, context) => {
+      return fetch(
+        `${THEMOVIEDB_API_PATH}/search/movie?api_key=${
+          context.secrets.THEMOVIEDB_API_KEY
+          }&query=${args.query}&language=en-US`
+      )
+        .then(res => res.json())
+        .then(({ results }) => results);
+    },
+    movie: (root, args, context) => {
+      return fetch(
+        `${THEMOVIEDB_API_PATH}/movie/${args.id}?api_key=${
+          context.secrets.THEMOVIEDB_API_KEY
+          }`
+      ).then(res => res.json());
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+
+if (!process.env.THEMOVIEDB_API_KEY) {
+  throw new Error(
+    "Please provide an API key for themoviedb.org in the environment variable THEMOVIEDB_API_KEY."
+  );
+}
+
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+
+// The GraphQL endpoint
+app.use("/graphql", bodyParser.json(),
+  express_graphql({
+    schema,
+    tracing: true,
+    graphiql: true,
+    cacheControl: true,
+    context: {
+      secrets: {
+        THEMOVIEDB_API_KEY: process.env.THEMOVIEDB_API_KEY
+      }
+    }
+  })
+);
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => console.log(`Express GraphQL Server Now Running On localhost:${PORT}/graphql`));
+
